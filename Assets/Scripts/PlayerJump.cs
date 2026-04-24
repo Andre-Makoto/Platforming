8d254190d5d9532f2f2dc6e55352d883dd0b24bd
Andre-Makoto <andremolito4@gmail.com>
2026-04-24 12:27:14 -0300

    Add ground-check, Ground tag and collision fix

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -6,6 +6,10 @@ public class PlayerJump : MonoBehaviour
     public float jumpForce = 300;
     public Rigidbody rigidBody;
     public Transform groundCheck;
+    public float groundCheckRadious = 0.2f;
+    public LayerMask groundLayer;
+
+    bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,12 +20,14 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadious, groundLayer);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rigidBody.AddForce(Vector3.up * jumpForce);
         }
+        Debug.Log(isGrounded);
     }
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Ground") 
         {
             Debug.Log("I stepped on somenthing!");
